06f0d2c04eed2237850231e4a3c818c7e43f25eb
jean3108 <jean.soler3108@gmail.com>
2020-09-28 11:06:28 +0200

    Change print information on the game

--- sticksGame/StateSG.py
+++ sticksGame/StateSG.py
@@ -25,6 +25,9 @@ class StateSG(StateGame):
         self.nbStick = nbStick
         self.maxPlayer = 1 if maxPlayer==True else -1
         self.nbMaxTook = nbMaxTook
+
+    def isOver(self):
+        return True if self.nbStick==0 else False
         
     def calculateScore(self):
         """
@@ -35,8 +38,8 @@ class StateSG(StateGame):
         :return: The score of the current state
         :rtype: int
         """
-        if(self.nbStick <= 1):
-            return -self.maxPlayer 
+        if(self.nbStick==0):
+            return self.maxPlayer 
         return 0 #To indicate no one win for now. Can be replace by a function which estimate the score
     
     def getChoices(self):
@@ -49,40 +52,40 @@ class StateSG(StateGame):
         nbMaxTurn = self.nbMaxTook if self.nbMaxTook<= self.nbStick else self.nbStick
         return [i for i in range(1, nbMaxTurn+1)]
         
-    def doChoice(self, nb, inNewState = False):
+    def doChoice(self, choice, inNewState = False):
         """
         Apply the choice to the current state (inplace or not)
         
-        :param nb: The number of stick that the player want to take
+        :param choice: The number of stick that the player want to take
         :param inNewState: To choose if the choice is apply inplace (on the current state) or not (on a copy of the current state)
         
-        :type nb: int
+        :type choice: int
         :type inNewState: boolean
         
         :return: Nothing if it's inplace then the new state.
         :rtype: StateSG or None
         """
         if(inNewState == False):
-            self.nbStick -= nb
+            self.nbStick -= choice
             self.maxPlayer *= -1
             return None
         else:
             return StateSG(self.nbStick-nb, -self.maxPlayer)
             
-    def undoChoice(self, nb, inNewState = False):
+    def undoChoice(self, choice, inNewState = False):
         """
         Undo the given choice for the current state (inplace or not)
         
-        :param nb: The number of stick that the player want to take
+        :param choice: The number of stick that the player want to take
         :param inNewState: To choose if the choice is apply inplace (on the current state) or not (on a copy of the current state)
         
-        :type nb: int
+        :type choice: int
         :type inNewState: boolean
         
         :return: Nothing if it's inplace then the new state.
         :rtype: StateSG or None
         """
-        return self.doChoice(-nb, inNewState)
+        return self.doChoice(-choice, inNewState)
     
     def toKey(self):
         """
@@ -94,3 +97,49 @@ class StateSG(StateGame):
         :rtype: string
         """
         return str(self.nbStick)+";"+str(self.maxPlayer)+";"+str(self.nbMaxTook)
+    
+    def printBeforeGame(self):
+        """
+        Print information before the beginning of the game
+        """
+        print("##############################\nThis is the game : Sticks Game\n##############################\n")
+        boardStr = ""
+        for i in range(self.nbStick):
+            boardStr += "|"
+        boardStr += " ("+str(self.nbStick)+" sticks)"
+        print(boardStr)
+    
+    def printInfoPlayer(self):
+        """
+        Print information before the turn of the current player
+        """
+        currentPlayer = 1 if self.maxPlayer==1 else 2
+        choices = self.getChoices()
+        print("\n################")
+        print("Turn of player",currentPlayer)
+        print("\nChoose your choice in this list (enter le number of the choosen item) :")
+        for i in range(len(choices)):
+            print(i+1,") Take",choices[i],"sticks")
+    
+    def printResultAction(self, choice):
+        """
+        Print information after the turn of the current player
+        
+        :param choice: The choice wich was just played
+            
+        :type choice: int
+        """
+        print("\nThe player takes",choice,"sticks from the board")
+        boardStr = ""
+        for i in range(self.nbStick):
+            boardStr += "|"
+        boardStr += " ("+str(self.nbStick)+" sticks)"
+        print(boardStr)
+    
+    def printAfterGame(self):
+        """
+        Print information after the end of the game
+        """
+        winner = 1 if self.calculateScore() else 2
+        print("\n#####################")
+        print("The winner is player", winner)
